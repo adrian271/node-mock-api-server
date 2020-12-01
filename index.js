d5a4af55abe78d69bfbccc916698c107643938d2
Adrian Barnes <adrian271@gmail.com>
2020-11-30 20:31:16 -0800

    ⏲ Add delay in route

--- index.js
+++ index.js
@@ -11,16 +11,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-  console.log('Request was made')
-  res.status(200).send({
-    success: {
-      status: 200,
-      msg: 'ok',
-    },
-  })
+  console.log(`Request: ${req.method} ${req.url}`)
+  setTimeout(() => {
+    res.status(200).send({
+      success: {
+        status: 200,
+        msg: 'ok',
+      },
+    })
+  }, 5000)
 })
 
 let port = process.env.PORT || 6625
 app.listen(port, () => {
-  console.log('api server started')
+  console.clear()
+  console.log(`API server started on port ${port}`)
 })
